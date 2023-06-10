ebfdb6af401f500ada336abae104e29df1c45a78
Miguel <migarcia@recursosformacion.com>
2023-06-10 10:51:57 +0200

    ss

---
+++ RepoConfig-1/src/test/java/com/repo/config/ClientConfig1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfig1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
